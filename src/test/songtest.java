67f137e3c9941d25e2e7a9cefd60aaefde1ed872
宋迎辉 <songyh_1314@163.com>
2017-10-14 18:09:18 +0800

    commit

---
+++ src/test/songtest.java
@@ -0,0 +1,12 @@
+package test;
+
+import org.omg.CORBA.PRIVATE_MEMBER;
+
+public class songtest {
+	
+	private String Name = "";
+	private Integer id = 0;
+	public static void main(String[] args) {
+	
+	}
+}
